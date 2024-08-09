4523f9985fa8d6b3c2592c4fe636e3dd6950a67c
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2024-08-09 16:35:50 +0700

    update

--- main.py
+++ main.py
@@ -2,47 +2,71 @@ import os
 import subprocess
 import time
 import sys
+import threading
+import psutil
+
+def pidExist(pid: int) -> bool:
+    return psutil.pid_exists(pid)
+
+def checkFound(pid: int) -> None:
+    while True:
+        if not pidExist(pid):
+            print("> program crashed!")
+            os.kill(main_pid, sig)
+        time.sleep(1)
+    return
 
 def getName(executable: str) -> str:
-	return "/".join(executable.split("\\")).split("/")[-1]
+    return "/".join(executable.split("\\")).split("/")[-1]
 
 def getPathExecutable(executable: str) -> str:
-	return subprocess.getoutput("where " + executable)
+    return subprocess.getoutput("where " + executable)
 
 def suspend(pid: int) -> None:
-	os.system("tools\\pssuspend64.exe " + str(pid) + " >nul 2>&1")
-	print("> suspended: PID: {}".format(pid))
-	return
+    os.system("tools\\pssuspend64.exe " + str(pid) + " >nul 2>&1")
+    print("> suspended: PID: {}".format(pid))
+    return
 
 def resume(pid: int) -> None:
-	os.system("tools\\pssuspend64.exe -r " + str(pid) + " >nul 2>&1")
-	print("> resume: PID: {}".format(pid))
-	return
+    os.system("tools\\pssuspend64.exe -r " + str(pid) + " >nul 2>&1")
+    print("> resume: PID: {}".format(pid))
+    return
 
 def run(executable: str, args: list) -> None:
-	p = __import__('subprocess').Popen([executable, *args]) #  creationflags=subprocess.CREATE_NEW_CONSOLE
-	print("> started: {} (PID: {})".format(getName(executable), p.pid))
-	return p
+    p = subprocess.Popen([executable, *args], close_fds=False, creationflags=subprocess.CREATE_NEW_CONSOLE)
+    print("> started: {} (PID: {})".format(getName(executable), p.pid))
+    return p
 
 def main() -> None:
-	print()
+    if (len(sys.argv) < 3):
+        print("main.py [second] [executable] [args]")
+        return
 
-	sec = sys.argv[1]
-	executable = sys.argv[2]
-	args = sys.argv[3:]
-	fullPath = getPathExecutable(executable)
+    sec = int(sys.argv[1])
+    executable = sys.argv[2]
+    args = sys.argv[3:]
+    fullPath = getPathExecutable(executable)
+    if not fullPath:
+        fullPath = executable
 
-	p = run(fullPath, args)
-	time.sleep(sec)
+    p = run(fullPath, args)
+    threading.Thread(target=checkFound, args=(p.pid,)).start()
 
-	suspend(p.pid)
+    print("> waiting for {} second....".format(sec))
+    time.sleep(sec)
 
-	print("> waiting for user.... press enter to resume!")
-	input()
+    suspend(p.pid)
 
-	resume(p.pid)
+    print("> waiting for user.... press enter to resume!")
+    input()
 
-	p.wait()
-	return
+    resume(p.pid)
 
-main()
+    p.wait()
+    return
+
+if os.name == 'nt':
+    sig = __import__('signal').SIGTERM
+    main_pid = os.getpid()
+    main()
+    os.kill(main_pid, sig)
